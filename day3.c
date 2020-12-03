9e89db26eb38c57a65925763584a5aa65a1aa92f
Paul Mc Auley <paul.mcauley@workday.com>
2020-12-03 10:28:46 +0000

    Day 3

---
+++ day3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  char buffer[512];
+  long count[5], row = 0;
+
+  if(argc == 1)
+  {
+    printf("%s <filename>\n", argv[0]);
+    exit(1);
+  }
+  if(!(fp = fopen(argv[1], "r")))
+  {
+    printf("Unable to open %s\n", argv[1]);
+    exit(2);
+  }
+  memset(count, 0, 5 * sizeof(long));
+
+  while(!feof(fp) && fgets(buffer, 256, fp))
+  {
+    int l = strlen(buffer) - 1;
+
+    if(buffer[row % l] == '#')
+      count[0]++;
+    if((row % 2 == 0) && (buffer[(row / 2) % l] == '#'))
+      count[4]++;
+    if(buffer[(3 * row) % l] == '#')
+      count[1]++;
+    if(buffer[(5 * row) % l] == '#')
+      count[2]++;
+    if(buffer[(7 * row) % l] == '#')
+      count[3]++;
+    row++;
+  }
+  printf("%ld * %ld * %ld * %ld * %ld = %ld\n",
+      count[0], count[1], count[2], count[3], count[4],
+      count[0] * count[1] * count[2] * count[3] * count[4]);
+}
