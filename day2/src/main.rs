1fb6a8e61827b6c8cb4b5e9cab633562cc785171
Paul Mc Auley <paul@gonzo.fritz.box>
2021-11-10 00:14:39 +0000

    Day 2 - Reader

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::vec::Vec;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if let Ok(lines) = read_lines(&args[1])
+    {
+        for line in lines
+        {
+            if let Ok(res) = line
+            {
+                println!("{}", res);
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
